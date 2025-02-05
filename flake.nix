887ad30247e93ee5f0e76bb3c1b05cf654d89d15
Raven <raven@rubicon.aleeas.com>
2025-02-04 18:20:01 -0600

    flake.nix: Fixed pkgs path

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    packages = forAllSystems (system: import ./personal/pkgs nixpkgs.legacyPackages.${system});
 
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
