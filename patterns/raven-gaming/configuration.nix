1cbfc0bbf491ee96715be3986f03e39cdd0f9271
Raven <raven@rubicon.aleeas.com>
2025-02-27 11:46:45 -0600

    Adding heroic optional dependencies as comments

--- patterns/raven-gaming/configuration.nix
+++ patterns/raven-gaming/configuration.nix
@@ -66,6 +66,21 @@
     heroic
   ];
 
+  # From https://wiki.nixos.org/wiki/Heroic_Games_Launcher
+  # Heroic has some optional dependencies, such as Gamescope and GameMode, that are not
+  # included in the FHS environment wrapper. If you want to use any of these, you need
+  # to override the Heroic derivation to pass extra packages.
+
+  #(heroic.override {
+  #  extraPkgs = pkgs: [
+  #    pkgs.gamescope
+  #  ];
+  #})
+
+  # For Gamescope and GameMode, you also need to enable these in your NixOS configuration.
+  #programs.gamescope.enable = true;
+  #programs.gamemode.enable = true;
+
   # Install steam
   programs.steam = {
     enable = true;
