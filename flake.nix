587d0737a86d7d43d066acc437080bca8bb46e64
Raven <raven@rubicon.aleeas.com>
2025-05-24 19:05:40 -0500

    Updated to nixos 24.11
    - Updated flake.lock file.
    - Required updates to flake.lock file and an (unwanted) alias for pcsxr.
    - Adding electron to insecure packages.

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     # Nixpkgs. Replace the text with your system.stateVersion in /etc/nixos/configuration.nix
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager. Replace the text with your system.stateVersion in /etc/nixos/configuration.nix
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
--- patterns/raven-gaming/emulators.nix
+++ patterns/raven-gaming/emulators.nix
@@ -15,15 +15,16 @@
     dolphin-emu  # GC / Wii
     mame         # Arcade
     mupen64plus  # N64
-    pcsxr        # PS1
     pcsx2        # PS2
     rpcs3        # PS3
     vbam         # Gameboy advance
     zsnes        # Super NES emulator
   ];
 
-  # Launch yuzu by running `raven-yuzu` and then `yuzu` in the resulting subshell.
+  # Launch these emulators using the alias followed by the emulator in the subshell.
+  # They have been removed from Nix's mainline repos for various reasons, but should still operate.
   programs.bash.shellAliases = {
     raven-yuzu = "nix-shell -p yuzuPackages.mainline -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/087f43a1fa052b17efd441001c4581813c34bc19.tar.gz";
+    raven-pcsxr = "nix-shell -p pcsxr -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/05bbf675397d5366259409139039af8077d695ce.tar.gz";
   };
 }
