fce55826878e3dd4cb9c5049b2476674187cdd99
Raven <raven@rubicon.aleeas.com>
2025-07-07 09:09:20 -0500

    raven-configure almost working; need to sed now
    Changed home-manager/master/default.nix to have one line for pattern,
    instead of multiple lines in which on is commented out.

--- patterns/raven-iso/configuration.nix
+++ patterns/raven-iso/configuration.nix
@@ -49,6 +49,8 @@
       writeShellScriptBin "install-raven"
       ''
         #!/usr/bin/env bash
+        # This script clones the raven-repository and runs raven-configure
+        # Before installation
         set -euo pipefail
 
         if [ "$(id -u)" -eq 0 ]; then
@@ -60,47 +62,7 @@
             git clone https://github.com/digital-raven/raven.git "$HOME/raven-machines"
         fi
 
-        cd $HOME/raven-machines
-
-        # Select hostname
-        HOSTNAME=$(gum input --prompt "What will you name your AC? " --placeholder "nightfall")
-
-        # Git username and email
-        if gum confirm --default=false "(Optional) Enter name and email you use for git commits?; then
-          GIT_USERNAME=$(gum input --prompt "Enter your name for git commits: ") --placeholder "Raven"
-          GIT_EMAIL=$(gum input --prompt "Enter your email for git commits: ") --placeholder "raven@rubicon.aleeas.com"
-        fi
-
-        # Select timezone
-        TIMEZONE=timedatectl list-timezones | gum choose --height=30
-
-        # Select locale
-        LOCALE="en_US.UTF-8"
-        if gum confirm --default=false "Select a locale other than $LOCALE? "; then
-          LOCALE=$(wget -qO - 'https://sourceware.org/git/?p=glibc.git;a=blob_plain;f=localedata/SUPPORTED' | sed -s 's/\\//' | tail -n +4 | gum choose --height=40 --header="Supported Locales")
-        fi
-
-        # Disk selection
-        sda_devices="$(lsblk -JpS -o NAME,VENDOR,MODEL,REV,SERIAL,SIZE)"
-        nvme_devices="$(lsblk -JpN -o NAME,VENDOR,MODEL,REV,SERIAL,SIZE)"
-
-        TARGET_DISK=$(echo "$sda_devices" \
-          "$nvme_devices" | jq -s '[.[].blockdevices[]] | map({name, vendor, model, rev, serial, size})' | jq -r '(.[0] | keys_unsorted) as $keys | $keys, map([.[ $keys[] ]])[] | @csv' | gum table) | awk -F, '{print $1}'
-
-        gum confirm  --default=false \
-          "This will erase $TARGET_DISK and format it for Raven. Is this correct?"
-
-        echo "Partitioning Disks"
-        sudo nix run github:nix-community/disko \
-        --extra-experimental-features "nix-command flakes" \
-        --no-write-lock-file \
-        -- \
-        --mode zap_create_mount \
-        "$HOME/dotfiles/hosts/$TARGET_HOST/disks.nix"
-
-        gum confirm --default=false "Install now?"
-
-        sudo nixos-install --flake "$HOME/#$TARGET_HOST"
+        $HOME/raven-machines/raven-configure
       ''
     )
   ];
