fe1ca235bcff1e7f26d828e1d7434d30c4b0083f
Raven <raven@rubicon.aleeas.com>
2026-04-19 19:09:46 -0500

    AMD GPU driver support

---
+++ host/drivers/amd.nix
@@ -0,0 +1,20 @@
+# Check the following URL to see if your GPU is supported by "open".
+# If your Nvidia GPU is after the 20-series, it probably is.
+#
+# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+{
+  pkgs,
+  config,
+  libs,
+  ...
+}: {
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+}
