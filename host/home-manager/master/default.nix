f582270536b122598afc281abbe2c7bbd4e90e38
Raven <raven@rubicon.aleeas.com>
2026-06-12 14:56:12 -0500

    Reverting previous commit

--- host/home-manager/master/default.nix
+++ host/home-manager/master/default.nix
@@ -10,6 +10,11 @@
 }: {
   # You can import other home-manager modules here
   imports = [
+    # If you want to use modules your own flake exports (from shared/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
 
     # Select the pattern you plan on using
     ../../../patterns/REPLACEME_raven-pattern/home-manager/default.nix
