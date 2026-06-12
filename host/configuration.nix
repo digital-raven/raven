afc244ef894c139c390fa4d82614e4397238a108
Raven <raven@rubicon.aleeas.com>
2026-06-12 14:53:49 -0500

    Removing shared directory
    Preserved on an experimental branch in case it finds use.

--- host/configuration.nix
+++ host/configuration.nix
@@ -18,9 +18,6 @@
   # inputs.hardware.nixosModules.common-ssd
 
   # You can also split up your configuration and import pieces of it here:
-
-  # If you want to use modules your own flake exports (from shared/modules):
-  # outputs.nixosModules.example
   imports = [
     # hardware-configuration.nix is brought to the system via flake.nix
 
@@ -66,6 +63,28 @@
 
   # Allow unfree software
   nixpkgs.config.allowUnfree = true;
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+
+      # Disable global registry
+      flake-registry = "";
+
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+
+    # Disable channels
+    channel.enable = false;
+
+    # Make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
