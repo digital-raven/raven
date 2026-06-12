e9e4ae15918e42bdc6582468de98fd89ed9bc345
Raven <raven@rubicon.aleeas.com>
2026-06-12 16:23:21 -0500

    shared bugfix

--- host/configuration.nix
+++ host/configuration.nix
@@ -67,6 +67,28 @@
   # Allow unfree software
   nixpkgs.config.allowUnfree = true;
 
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+
+      # Disable global registry
+      flake-registry = "";
+
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+
+    # Disable channels
+    channel.enable = false;
+
+    # Make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
 }
