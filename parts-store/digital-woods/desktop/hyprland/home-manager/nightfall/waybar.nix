c60fb258479ef91f8c7127fd6cf978c96de1cf16
Raven <raven@rubicon.aleeas.com>
2026-06-20 21:27:19 -0500

    Redirected inputs to own forks
    All inputs now redirect to forks of nixpkgs, home-manager, and
    disko. These forks are updated these revisions.
    
    - nixpkgs: End-of-life commit for 25.05.
    - home-manager: release-25.05. Should also be end-of-life.
    - disko: v1.13.0 from v1.12.0
    
    interesting: disko's hash updated, but the others didn't. I
    cross-referenced the repository and could not find a reference
    to the hash before this commit.
    
    before: ff442f5d1425feb86344c028298548024f21256d
    after: a956a7c25513db89d0d8a7ac22c721423080d4aa
    
    There is also no v1.12.0 tag in the upstream repo, which leads me to
    wonder if this tag was yanked, or the git history modified, for any
    reason.
    
    It also leads me to wonder what this would do to new builds who
    relied on the v1.12.0 ref? Glad we don't need to worry about those
    problems.
    
    waybar
    ------
    Waybar is pinned to version 0.12.0.
    
    Waybar v0.14.0 has a bug causing wildcard includes to be incorrectly
    parsed. This breaks large numbers of waybar configurations.
    
    It was patched in Waybar v0.15.0, but this version was not available in
    the NixOS 25.05 branch of nixpkgs.
    
    Permitted insecure packages
    ---------------------------
    2 insecure packages were permitted; justified by the associated
    applications. The associated packages will be evaluated for removal.
    
    - "electron-36.9.5" # heroic games launcher.
    - "python3.12-ecdsa-0.19.1" # electrum requirement.

--- parts-store/digital-woods/desktop/hyprland/home-manager/nightfall/waybar.nix
+++ parts-store/digital-woods/desktop/hyprland/home-manager/nightfall/waybar.nix
@@ -1,13 +1,33 @@
 # This waybar config relies on some extra programs.
-{pkgs, ...}: {
-  programs.waybar.enable = true;
+{
+  config,
+  pkgs,
+  ...
+}: let
+  pkgs_waybar_0_12_0 =
+    import (pkgs.fetchFromGitHub {
+      owner = "nixos";
+      repo = "nixpkgs";
+      rev = "7282cb574e0607e65224d33be8241eae7cfe0979";
+      hash = "sha256-hYKMs3ilp09anGO7xzfGs3JqEgUqFMnZ8GMAqI6/k04=";
+    }) {
+      inherit (pkgs) system;
+    };
+in {
+  # Specific version of waybar
+  programs.waybar = {
+    enable = true;
+    package = pkgs_waybar_0_12_0.waybar;
+  };
 
   # Brightness and audio control
   home.packages = with pkgs; [
+    # Regular packages
     brightnessctl
     pamixer
     pulseaudio # Installs pactl
   ];
+
   services.playerctld.enable = true;
 
   home.file = {
